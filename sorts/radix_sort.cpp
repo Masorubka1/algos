e9c2801ef86ebcda3ba08396b214d5274e380167
Masorubka1 <43664538+Masorubka1@users.noreply.github.com>
2020-04-28 12:04:11 +0300

    Create radix_sort.cpp

---
+++ sorts/radix_sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void radix_sort(vector <int> &a, int len)
+{
+    if (a[0] / int(pow(10, len)) == 0)
+        return;
+    vector <vector <int>> cnt(10);
+    for (int i = 0; i < a.size(); ++i)
+    {
+        int num = a[i];
+        int last_need = (num / int(pow(10, len))) % 10;
+        cnt[last_need].push_back(i);
+    }
+    vector <int> help;
+    int i = 0;
+    while (i < 10)
+    {
+        int j = 0;
+        while  (j < cnt[i].size())
+        {
+            int ind = cnt[i][j];
+            help.push_back(a[ind]);
+            j++;
+        }
+        i++;
+    }
+    a = help;
+    radix_sort(a, len + 1);
+}
+
+int main()
+{
+    vector <int> a = {329, 457, 657, 839, 436, 720, 335};
+    radix_sort(a, 0);
+    for (int i = 0; i < a.size(); ++i)
+    {
+        cout << a[i] << " ";
+    }
+}
