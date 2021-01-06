f4ca9adc935111efc9047f1937a05a8d5b06c027
a.maltsev <maltsev.a2012@gmail.com>
2021-01-06 14:58:12 +0300

    gg

--- structs/list.cpp
+++ structs/list.cpp
@@ -14,7 +14,8 @@ public:
     };
     List(int n = 0)
     {
-        start = new _list;
+        start = nullptr;
+        /*start = new _list;
         _list* cur = start;
         _list* prev = nullptr;
         capacity = 1;
@@ -25,38 +26,42 @@ public:
             prev= cur;
             cur = cur->next;
             capacity++;
-        }
+        }*/
     }
     void push(T elem)
     {
         _list* cur = start;
         _list* prev = nullptr;
-        while (cur != nullptr || cur->elem == nullptr)
+        if (start == nullptr)
+        {
+            start = new _list;
+            start->elem = elem;
+            start->next = nullptr;
+            start->prev = nullptr;
+            capacity++;
+            return;
+        }
+        while (cur != nullptr)
         {
             prev = cur;
             cur = cur->next;
         }
-        if (cur == nullptr)
-        {
-            cur = new _list;
-            cur->elem = elem;
-            prev-> next = cur;
-            cur->prev = prev;
-            capacity++;
-        }
-        else
-        {
-            cur->elem = elem;
-        }
+        cur = new _list;
+        cur->elem = elem;
+        prev->next = cur;
+        cur->prev = prev;
+        capacity++;
     }
-    void pop(T elem)
+    T pop()
     {
         _list* cur = start;
-        while (cur->elem != elem && cur != nullptr)
+        _list* prev = nullptr;
+        while (cur->next != nullptr)
         {
+            prev = cur;
             cur = cur->next;
         }
-        if (cur->elem == elem)
+        /*if (cur->elem == elem)
         {
             if (capacity == 1)
             {
@@ -76,13 +81,43 @@ public:
                 cur->prev->next = cur->next;
                 cur->next->prev = cur->prev;
             }
+            T el = cur->elem;
             delete cur;
+            return el;
         }
         else
         {
             cerr << "Warning there is nothing to pop\n";
+            return 0;
+        }*/
+        if (prev == nullptr)
+        {
+            start = nullptr;
         }
-
+        T el = cur->elem;
+        if (prev != nullptr)
+            prev->next = nullptr;
+        capacity--;
+        return el;
+    }
+    void reverse()
+    {
+        if (start == nullptr)
+        {
+            return;
+        }
+        struct _list* curr;
+        struct _list* next;
+        struct _list* prev = nullptr;
+        curr = start;
+        while(curr)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        start = prev;
     }
     ~List()
     {
@@ -95,15 +130,15 @@ public:
                 q.push(cur);
                 cur = cur->next;
             }
-            else
+            else if (capacity > 0)
             {
                 q.pop();
                 capacity--;
             }
         }
-        while (!q.empty())
+        while (!q.empty());
     }
 private:
-    _list start;
+    _list* start;
     int capacity;
 };
